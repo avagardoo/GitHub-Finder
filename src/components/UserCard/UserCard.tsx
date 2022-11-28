0fe7ba8ac7934868146f883c9fa5d5b735b67466
Ivan Mikheev <avagardo@mail.ru>
2022-11-28 17:04:59 +0300

    Create Title
    Create title n fix sass

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,13 +1,31 @@
 import styles from './UserCard.module.scss';
 import {LocalGithubUser} from "../../types";
 import {UserStat} from "../UserStat";
+import {UserTitle} from "../UserTitle";
 
 interface UserCardProps extends LocalGithubUser  { }
 
-export const UserCard = (props: UserCardProps) => (
-  <div className={styles.userCard}>
-    <UserStat repos={props.repos}
-              followers={props.followers}
-              following={props.following}/>
-  </div>
-);
+
+export const UserCard = (props: UserCardProps) => {
+
+    return (
+        <div className={styles.userCard}>
+                    <img src={props.avatar}
+                     alt={props.login}
+                     className={styles.avatar}
+            />
+            <UserTitle login={props.login}
+                       name={props.name}
+                       created={props.created}
+            />
+            <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+                {props.bio || 'This profile has no bio'}
+            </p>
+            <UserStat repos={props.repos}
+                      followers={props.followers}
+                      following={props.following}
+            />
+        </div>
+    );
+};
+
