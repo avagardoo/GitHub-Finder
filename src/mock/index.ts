351c98ef2d2df13817f5c8c3e479b9313d752f0c
Ivan Mikheev <avagardo@mail.ru>
2022-11-28 12:04:08 +0300

    Create Card
    Create card // Broken Styles

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import {LocalGithubUser} from "../types";
+
+export const  defaultUser: LocalGithubUser = {
+    "login": "avagardoo",
+    "id": 69362379,
+    "avatar": "https://avatars.githubusercontent.com/u/69362379?v=4",
+    "name": "Ivan Mikheev",
+    "company": "AV",
+    "blog": "",
+    "location": "",
+    "bio": "",
+    "twitter": "",
+    "repos": 2,
+    "followers": 0,
+    "following": 0,
+    "created": "2020-08-07T17:39:00Z",
+}
